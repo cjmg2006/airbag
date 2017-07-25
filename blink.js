f334f7b14c21446d945fad014f80a35298e4f794
Gloria Chua <cjmg2006@gmail.com>
2017-07-25 16:26:41 -0700

    successfully scans 3 tags simultaneously and checks it on the Merkle roots

--- blink.js
+++ blink.js
@@ -20,15 +20,79 @@ var componentB = "3DC027D13824"
 var componentC = "2DC033D14233"
 var componentD = "3DC027D1fake"
 var componentE = "2DC033D1fake"
-
 var componentBank = { 'A': componentA, 'B': componentB, 'C': componentC, 'D': componentD, 'E': componentE }
 
+var realAirbagRoots = []; 
+
+// Merkle tree related variables 
+var treeOptions = {
+  hashType: 'sha256' // optional, defaults to 'sha256'
+};
+
 
 /*******************************************/
-// Part A: Generate initial Merkle Tree
+// Part A1: Merkle Tree Functions 
 /*******************************************/
 
+function convertToComponentString(a) { 
+  return componentBank[a];
+}
 
+// Function: Gets 3 alphanumeric letters and returns a Merkle tree built from the corresponding components 
+function createMerkleTreeAlpha(array) {
+	var components = [convertToComponentString(array[0]), convertToComponentString(array[1]), convertToComponentString(array[2])];
+	var tree = new MerkleTools(treeOptions); 
+
+	// TODO: add proofs to each individual leaf? 
+
+	return createMerkleTree (components); 
+}
+
+function createMerkleTree(components) { 
+	var tree = new MerkleTools(treeOptions); 
+	tree.addLeaves(components, true); 
+	tree.makeTree();
+
+	return tree; 
+} 
+
+function generateCombinations(a, b, c) {
+  var result = []; 
+  result.push([a, b, c]); 
+  result.push([a, c, b]); 
+  result.push([b, a, c]); 
+  result.push([b, c, a]); 
+  result.push([c, a, b]); 
+  result.push([c, b, a]); 
+  return result;
+}
+
+function createMerkleRoots(a, b, c) { 
+  var result = [];
+  var combinations = generateCombinations(a, b, c);
+  for (var i = 0; i < combinations.length; i++) { 
+    var combo = combinations[i];
+    var tree = createMerkleTreeAlpha(combo);
+    var root = tree.getMerkleRoot(); 
+    result.push(root);
+  } 
+  return result;
+}
+
+function verifyRoot(r, roots)  {
+  for (var i = 0; i < roots.length; i++) {
+    var root = roots[i];
+    if(r.equals(root)) return true;
+  }
+
+  return false;
+}
+
+/*******************************************/
+// Part A2: Generate Merkle Tree for Legits
+/*******************************************/
+realAirbagRoots = createMerkleRoots('A', 'C', 'E');
+// console.log(realAirbagRoots);
 
 /*******************************************/
 // Part B: Reading from Arduino code 
@@ -50,7 +114,7 @@ function openFn() {
 }
 
 function dataFn(data) {
-	console.log(data);
+	// console.log(data);
 	 
 	if (data ===  "Scanning\r" || data === "Go!\r" || data === "Module continuously reading. Asking it to stop...\r") {
 		 
@@ -58,7 +122,8 @@ function dataFn(data) {
 		 
 		numTagsRead++;
 		EPCTagStrings.push(data); 
-		console.log(EPCTagStrings);
+		console.log("Num tags read: " + numTagsRead);
+		// console.log(EPCTagStrings);
 	}
 
 	if(numTagsRead == 3) {
@@ -84,11 +149,19 @@ function processTags() {
 		var tagString = EPCTagStrings[i]; 
 		var tagChars = tagString.split(" ");
 		tagChars.splice(-1,1);
-		console.log(tagChars);
+		// console.log(tagChars);
 		var tag = hexToAscii(tagChars);  // tag is a 12-char string e.g. "ST31D12dEWG"
 		EPCTags.push(tag); 
 	}
 	// console.log(EPCTags);
+}
+
+// Function: takes the EPC tags stored in the global array, creates a Merkle root, and checks whether it exists in realAirbagRoots
+function checkAgainstMerkleTree() { 
+	var tree = createMerkleTree(EPCTags); 
+	var root = tree.getMerkleRoot(); 
+	var match = verifyRoot(root, realAirbagRoots); 
+	console.log("Do we have the correct parts? " + match);
 }
 
 function errorFn() {
